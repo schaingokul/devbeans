fa932d2ec791921d71a090f6ecdcb079eba3693f
Gokul <schaingokul@gmail.com>
2026-01-28 16:22:36 +0530

    Update Next.js 16 config and remove deprecated eslint option

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
 };
 
-module.exports = nextConfig;
-
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
+export default nextConfig;
